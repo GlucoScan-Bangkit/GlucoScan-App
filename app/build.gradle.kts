90f1a4ab3dff3fa92a354e739bbc8eba8771060c
Narapati24 <narapati.223040155@mail.unpas.ac.id>
2024-11-28 14:43:20 +0700

    feat: perbarui kata sandi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.material.v190)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/dicoding/glucoscan/ui/screen/usereditor/PasswordEditorActivity.kt
+++ app/src/main/java/com/dicoding/glucoscan/ui/screen/usereditor/PasswordEditorActivity.kt
@@ -1,6 +1,7 @@
 package com.dicoding.glucoscan.ui.screen.usereditor
 
 import android.os.Bundle
+import android.text.InputType
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +15,17 @@ class PasswordEditorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPasswordEditorBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//        set input text
+        binding.passwordInput.title.text = getString(R.string.password)
+        binding.passwordInput.input.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+
+        binding.newPasswordInput.title.text = getString(R.string.new_password)
+        binding.newPasswordInput.input.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+
+        binding.confirmationPasswordInput.title.text = getString(R.string.confirmation_password)
+        binding.confirmationPasswordInput.input.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+
         binding.icBack.setOnClickListener{
             finish()
         }
