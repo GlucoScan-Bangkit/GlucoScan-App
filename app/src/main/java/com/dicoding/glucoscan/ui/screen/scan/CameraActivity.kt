d17792c5612276ae2033a3188d62384d5bbd465c
Narapati24 <narapati.223040155@mail.unpas.ac.id>
2024-11-29 14:27:24 +0700

    fix: photo rotation

--- app/src/main/java/com/dicoding/glucoscan/ui/screen/scan/CameraActivity.kt
+++ app/src/main/java/com/dicoding/glucoscan/ui/screen/scan/CameraActivity.kt
@@ -6,6 +6,8 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.OrientationEventListener
+import android.view.Surface
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Toast
@@ -25,6 +27,27 @@ class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
     private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
     private var imageCapture: ImageCapture? = null
+
+    private val orientationEventListener by lazy {
+        object : OrientationEventListener(this){
+            override fun onOrientationChanged(orientation: Int) {
+                if (orientation == ORIENTATION_UNKNOWN){
+                    return
+                }
+
+                val rotation = when (orientation){
+                    in 45 until 135 -> Surface.ROTATION_270
+                    in 135 until 225 -> Surface.ROTATION_180
+                    in 225 until 315 -> Surface.ROTATION_90
+                    in 315 until 360 -> Surface.ROTATION_0
+                    else -> Surface.ROTATION_0
+                }
+
+                imageCapture?.targetRotation = rotation
+            }
+
+        }
+    }
 
     private val requestPermissionLauncher =
         registerForActivityResult(
@@ -56,10 +79,20 @@ class CameraActivity : AppCompatActivity() {
         binding.captureImage.setOnClickListener { takePhoto() }
     }
 
+    override fun onStart() {
+        super.onStart()
+        orientationEventListener.enable()
+    }
+
     public override fun onResume() {
         super.onResume()
         hideSystemUI()
         startCamera()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        orientationEventListener.disable()
     }
 
     private fun startGallery() {
